5168fed477048cc8e550cf01edd9645643954a01
GS04-ID-nishijima <kurasshu@qc4.so-net.ne.jp>
2016-08-07 19:51:47 +0900

    トップページの作成分をコミット、未完了 ライブラリとして、カルーセルのslick、写真モーダルのlightboxを追加

--- web/js/main.js
+++ web/js/main.js
@@ -1,18 +1,9 @@
-function initMap() {
-    try {
-        // 初期表示地
-        var lat = 35.648971;
-        var lon = 139.734873;
-
-        marker_list = new google.maps.MVCArray();
-
-        //div#mapを「GoogleMap」化
-        map = new google.maps.Map(document.getElementById('hostinfomap'), {
-           center: {lat: lat, lng: lon},
-           zoom: 13
-        });
-
-    } catch (error) {
-        console.log("getGeolocation: " + error);
-    }
-};
+function formatHostinfoDate(holdingDateYmd) {
+    var objDate = new Date(holdingDateYmd.substring(0, 4) + "/" + holdingDateYmd.substring(4, 6) + "/" + holdingDateYmd.substring(6));
+    var month = objDate.getMonth() + 1;
+    var day = objDate.getDate();
+    var dayOfWeek = objDate.getDay();
+    var week = new Array("日","月","火","水","木","金","土");
+    
+    return "" + month + "月" + day + "日（" + week[dayOfWeek] + "）開催";
+}
